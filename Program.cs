ecc83531a18d6dcd0502a695e6bee94f25b80e41
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-10 08:11:01 +0600

    Revert "TooWayLongWord problem solved"
    This reverts commit 3453161bd2da171a35315081d0d0a8ff04255986.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
-﻿var inputNumber = Convert.ToInt16(Console.ReadLine());
-if (inputNumber>2 && inputNumber % 2 == 0)
+﻿// See https://aka.ms/new-console-template for more information
+var inputNumber = Convert.ToInt16(Console.ReadLine());
+List<string> wordStringArray = new List<string>();
+
+for (int i = 0; i < inputNumber; i++)
 {
-    Console.WriteLine("YES");
+    wordStringArray.Add(Console.ReadLine());
 }
-else {
-    Console.WriteLine("NO");
+
+for (int i = 0; i < inputNumber; i++)
+{
+    if (Convert.ToInt16(wordStringArray[i].Count()) <= 10)
+    {
+        Console.WriteLine(wordStringArray[i]);
+    }
+    else
+    {
+        var firstCharacter = wordStringArray[i].Substring(0, 1);
+        var secondCharacter = wordStringArray[i].Substring(wordStringArray[i].Length - 1);
+        var characterCount = wordStringArray[i].Count() - 2;
+        Console.WriteLine(firstCharacter + characterCount + secondCharacter);
+    }
 }
 
