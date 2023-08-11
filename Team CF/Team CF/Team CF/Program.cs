2a3d637ff47c6456edd10d59451c0cc3e8d83534
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-11 16:53:56 +0600

    Team problem solved

--- Team CF/Team CF/Team CF/Program.cs
+++ Team CF/Team CF/Team CF/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var inputTimes = Convert.ToInt16(Console.ReadLine());
+
+var outerSum = 0;
+
+for (int j = 0; j < inputTimes; j++)
+{
+    var innerSum = 0;
+    var firstInput = Console.ReadLine();
+
+    var firstNumber = firstInput.Substring(0, 1);
+    var secondNumber = firstInput.Substring(2, 2);
+    var thiredNumber = firstInput.Substring(firstInput.Length - 1);
+
+    if (Convert.ToInt16(firstNumber) == 1)
+    {
+        innerSum += 1;
+    }
+
+    if (Convert.ToInt16(secondNumber) == 1)
+    {
+        innerSum += 1;
+    }
+
+    if (Convert.ToInt16(thiredNumber) == 1)
+    {
+        innerSum += 1;
+    }
+
+    if (innerSum >= 2)
+    {
+        outerSum += 1;
+    }
+
+}
+
+Console.WriteLine(outerSum);
+
 
