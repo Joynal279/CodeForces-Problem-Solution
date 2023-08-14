95a63f1709ee5b67b4516c0c25b1b32194a8a516
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-14 09:36:39 +0600

    Next round CF problem solved

--- NextRound CF/NextRound CF/Program.cs
+++ NextRound CF/NextRound CF/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var foundNumber = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+var range = foundNumber[1];
+var inputRange = foundNumber[0];
+
+int[] arr = new int[inputRange];
+
+arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+var positionNumber = arr[range - 1];
+
+var sum = 0;
+
+for (int i = 0; i < arr.Count(); i++)
+{
+    if (arr[i] >= positionNumber && arr[i] > 0)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine(sum);
+
 
