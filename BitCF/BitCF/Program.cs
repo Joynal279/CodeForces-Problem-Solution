6f8ad8c13b05a71b77b5ab5472b16249a88613c4
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-18 12:07:48 +0600

    Bit problem solved

--- BitCF/BitCF/Program.cs
+++ BitCF/BitCF/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var loopNumber = Convert.ToInt32(Console.ReadLine());
+var sum = 0;
+for (int i = 0; i<loopNumber; i++)
+{
+    var inputBit = Console.ReadLine();
+    if (inputBit == "++x" || inputBit == "x++")
+    {
+        sum = sum + 1;
+    }
 
+    if (inputBit == "--x" || inputBit == "x--")
+    {
+        sum = sum - 1;
+    }
+}
+Console.WriteLine(sum);
