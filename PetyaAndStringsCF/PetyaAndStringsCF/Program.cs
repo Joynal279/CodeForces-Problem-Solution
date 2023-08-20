351131643f03704a0931dbf639adb540dd6d39bb
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-20 08:42:21 +0600

    problem solved

--- PetyaAndStringsCF/PetyaAndStringsCF/Program.cs
+++ PetyaAndStringsCF/PetyaAndStringsCF/Program.cs
@@ -1,6 +1,21 @@
-﻿var inputOne = Console.ReadLine();
-var inputTwo = Console.ReadLine();
+﻿var inputOne = Console.ReadLine().ToLower();
+var inputTwo = Console.ReadLine().ToLower();
 
-var output = string.Compare(inputTwo, inputOne);
-Console.WriteLine(output);
+if (String.Compare(inputOne, inputTwo) < 0)
+{
+    // str1 is less than str2
+    Console.WriteLine("-1");
+}
+else if (String.Compare(inputOne, inputTwo) == 0)
+{
+    // str1 is less than str2
+    Console.WriteLine("0");
+}
+else
+{
+    Console.WriteLine("1");
+}
+
+//var output = string.Compare(inputTwo, inputOne);
+//Console.WriteLine(output);
 
