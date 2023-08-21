7ab8d7dd402d42a715f3a534b65dcab76d929590
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-22 00:44:46 +0600

    BoyOrGirl problem solved

--- BoyOrGirlCF/BoyOrGirlCF/Program.cs
+++ BoyOrGirlCF/BoyOrGirlCF/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myStr = Console.ReadLine();
+var unique = new HashSet<char>(myStr);
+
+var sum = 0;
+
+foreach (char c in unique)
+{
+    sum += 1;
+}
+
+if (sum%2 == 0)
+{
+    Console.WriteLine("CHAT WITH HER!");
+}
+else
+{
+    Console.WriteLine("IGNORE HIM!");
+}
+    
 
