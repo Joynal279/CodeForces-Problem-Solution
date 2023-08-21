3fb60ec4aaf2302d21fa12e6cfe426459e5e6f25
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-21 08:24:46 +0600

    HelpfulMaths problem solved

--- HelpfulMathsCF/HelpfulMathsCF/Program.cs
+++ HelpfulMathsCF/HelpfulMathsCF/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var a = Array.ConvertAll(Console.ReadLine().Trim().Split('+'), Convert.ToInt32);
 
+for (int i = 0; i < a.Count(); i++)
+{
+    for (int j = 0; j < (a.Count() - 1); j++)
+    {
+        if (a[j] > a[j + 1])
+        {
+            int temp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = temp;
+        }
+    }
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    if (i == a.Length-1)
+    {
+        Console.Write(a[i]);
+    }
+    else
+    {
+        Console.Write(a[i] + "+");
+    }
+}
