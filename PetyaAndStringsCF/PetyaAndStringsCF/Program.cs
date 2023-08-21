5ed67e0e8b4b4f7239a501ea56ab97e446d55f75
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-21 07:09:04 +0600

    Helpful maths initialize

--- PetyaAndStringsCF/PetyaAndStringsCF/Program.cs
+++ PetyaAndStringsCF/PetyaAndStringsCF/Program.cs
@@ -3,12 +3,10 @@ var inputTwo = Console.ReadLine().ToLower();
 
 if (String.Compare(inputOne, inputTwo) < 0)
 {
-    // str1 is less than str2
     Console.WriteLine("-1");
 }
 else if (String.Compare(inputOne, inputTwo) == 0)
 {
-    // str1 is less than str2
     Console.WriteLine("0");
 }
 else
@@ -16,6 +14,4 @@ else
     Console.WriteLine("1");
 }
 
-//var output = string.Compare(inputTwo, inputOne);
-//Console.WriteLine(output);
 
