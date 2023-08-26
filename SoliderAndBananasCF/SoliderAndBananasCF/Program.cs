f9928f8276cd50611096995bbb2858a009ce4472
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-26 16:09:03 +0600

    SoliderAndBananasCF problem solved

---
+++ SoliderAndBananasCF/SoliderAndBananasCF/Program.cs
@@ -0,0 +1,20 @@
+﻿var inputNumber = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+var price = inputNumber[0];
+var tkHave = inputNumber[1];
+var totalBanana = inputNumber[2];
+
+var totalMoney = 0;
+
+for(int i = 1; i <= inputNumber[2]; i++)
+{
+    totalMoney += i * price;
+}
+
+if (totalMoney > tkHave)
+{
+    Console.WriteLine(totalMoney - tkHave);
+}
+else
+{
+    Console.WriteLine("0");
+}
