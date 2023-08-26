1bbd440a8228b0f3b5e65626158ddcba8225d9ee
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-26 23:52:19 +0600

    WordCF problem solved

---
+++ WordCF/WordCF/Program.cs
@@ -0,0 +1,24 @@
+﻿var inputWord = Console.ReadLine();
+
+
+var totalChar = 0;
+var totalUpper = 0;
+
+foreach(char c in inputWord)
+{
+    totalChar += 1;
+    if (char.IsUpper(c))
+    {
+        totalUpper += 1;
+    }
+}
+
+if (totalUpper > (totalChar - totalUpper))
+{
+    Console.WriteLine(inputWord.ToUpper());
+}
+else
+{
+    Console.WriteLine(inputWord.ToLower());
+}
+
