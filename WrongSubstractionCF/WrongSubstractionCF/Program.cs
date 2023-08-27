73d961518554872473ba94b07791d5c64f5721b8
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-27 19:00:46 +0600

    problem solved

---
+++ WrongSubstractionCF/WrongSubstractionCF/Program.cs
@@ -0,0 +1,16 @@
+﻿var inputNumber = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+var firstNumber = inputNumber[0];
+var secondNumber = inputNumber[1];
+
+for(int i = 1; i<=secondNumber; i++)
+{
+    if (firstNumber % 10 == 0)
+    {
+        firstNumber = firstNumber / 10;
+    }
+    else
+    {
+        firstNumber = firstNumber - 1;
+    }
+}
+Console.WriteLine(firstNumber);
