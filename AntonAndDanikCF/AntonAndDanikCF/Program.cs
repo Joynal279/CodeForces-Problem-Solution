d779c6177a4037aab67e1ef2b7fea398fd09633b
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-01 15:56:41 +0600

    problem solved

---
+++ AntonAndDanikCF/AntonAndDanikCF/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var loopNumber = Convert.ToInt64(Console.ReadLine());
+var inputString = Console.ReadLine().ToUpper();
+var antonCount = 0;
+var danikCount = 0;
+
+for (int i = 0; i < loopNumber; i++)
+{
+    if (inputString[i] == 'A')
+    {
+        antonCount += 1;
+    }
+    if (inputString[i] == 'D')
+    {
+        danikCount += 1;
+    }
+}
+if(antonCount == danikCount)
+{
+    Console.WriteLine("Friendship");
+}
+if (antonCount > danikCount)
+{
+    Console.WriteLine("Anton");
+}
+if (danikCount > antonCount)
+{
+    Console.WriteLine("Danik");
+}
