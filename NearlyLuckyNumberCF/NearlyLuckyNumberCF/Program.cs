f0803c72ade705a81ee584449f06f3a188dfb372
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-01 06:06:55 +0600

    problem solved

--- NearlyLuckyNumberCF/NearlyLuckyNumberCF/Program.cs
+++ NearlyLuckyNumberCF/NearlyLuckyNumberCF/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var inputNumber = Console.ReadLine();
+
+var count = 0;
+
+foreach(char c in inputNumber)
+{
+    if(c == '4' || c == '7')
+    {
+        count += 1;
+    }
+}
+
+if(count == 4 || count == 7)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
 
