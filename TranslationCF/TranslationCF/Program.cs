8ba8a583e5c356f0229a9fcf1ddc164bee90f524
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-09 13:52:02 +0600

    TranslationCF problem solved

---
+++ TranslationCF/TranslationCF/Program.cs
@@ -0,0 +1,18 @@
+﻿var firstInput = Console.ReadLine().ToString();
+var secondInput = Console.ReadLine().ToString();
+
+char[] stringArray = secondInput.ToCharArray();
+Array.Reverse(stringArray);
+string reversedStr = new string(stringArray);
+
+if (firstInput == reversedStr)
+{
+    Console.WriteLine(reversedStr);
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine(reversedStr);
+    Console.WriteLine("NO");
+}
+
